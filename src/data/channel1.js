cc7e8331ecff8cd86e41b521ba099fd0916df6fa
Ben Christel <ben.christel@gmail.com>
2024-03-07 19:29:49 -0800

    Cut less aggressively

--- src/data/channel1.js
+++ src/data/channel1.js
@@ -4,13 +4,14 @@ export const episodes: Array<string> = []
 
 export const videos: string = `
 7KllsOwtHK0 4:45-4:48 Ask Adam Savage: Ark of the Covenant Replicas and Duct Tape Bullwhips
-6HOWrW4p-Xg 8:26-9:14 The point of storage is to know where everything is
-UUNBBNLCqmU 0-1:22,2:02-2:14 In order to assess a technique, I have to have already thought how I would do it
+6HOWrW4p-Xg 7:19-10:57 The point of storage is to know where everything is
+UUNBBNLCqmU 0-2:14 In order to assess a technique, I have to have already thought how I would do it
 UUNBBNLCqmU 2:25-3:17 The give and take of collaboration
 qQlF7upgTlA 2:20-8:05 Ask Adam Savage: "What Do You Think Will Be Your Legacy?"
 qQlF7upgTlA 8:07-8:33 Ask Adam Savage: "What Do You Think Will Be Your Legacy?"
 qQlF7upgTlA 8:49-9:15 Ask Adam Savage: "What Do You Think Will Be Your Legacy?"
-5CRSXcoasQQ 1:40-5:25 If I could wave a magic wand
+5CRSXcoasQQ 0:00-5:25 If I could wave a magic wand
 5CRSXcoasQQ 6:23-7:10 You can start late and be great
-exCrXc8hAAA 0:57-2:29 No amount of whiteboarding prepares you for just starting
+exCrXc8hAAA 0:00-2:29 No amount of whiteboarding prepares you for just starting
+cgFmFqh90PU 1:08-6:01 Ask Adam Savage: MythBusters' Unsung Heroes
 `
