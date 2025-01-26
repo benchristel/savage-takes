4a6ab3887e41d75d7e18efd049c470b7522167f8
Ben Christel <ben.christel@gmail.com>
2025-01-26 12:58:18 -0700

    Trim video

--- src/data/channel1.js
+++ src/data/channel1.js
@@ -15,6 +15,6 @@ qQlF7upgTlA 8:49-9:15 Ask Adam Savage: "What Do You Think Will Be Your Legacy?"
 exCrXc8hAAA 0:00-2:29 No amount of whiteboarding prepares you for just starting
 cgFmFqh90PU 1:08-6:01 Ask Adam Savage: MythBusters' Unsung Heroes
 T5fjMnfSSbQ 0:00-4:03 Plan upfront or wing it?
-H6yVtuPG1lc 0:00-13:57 "objects of power bring their own moral compass - you can't control them"
+H6yVtuPG1lc 4:49-8:27 "objects of power bring their own moral compass - you can't control them"
 O1--0DLJv-8 8:39-9:08 If you make sure you're not lazy in the beginning, you might give yourself an opportunity to be lazy later, when you're tired.
 `
